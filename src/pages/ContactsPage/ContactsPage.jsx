8acb5ece5167478d0580ec52298957c275d9fb5a
Krasnozhon Roman <105301264+KraRo16@users.noreply.github.com>
2023-02-01 01:23:01 +0100

    add show btn

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,13 +1,49 @@
 import ContactForm from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'components/Button/Button';
+import { fetchContacts } from 'redux/contacts/contacts-operations';
+import {
+  // selectIsLoading,
+  selectError,
+} from 'redux/contacts/contacts-selectors';
 
 const ContactsPage = () => {
+  const [isListShown, setIsListShown] = useState(false);
+  const [isFormShown, setIsFormShown] = useState(false);
+  const dispatch = useDispatch();
+  // const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  const showContactsList = () => {
+    setIsListShown(true);
+    dispatch(fetchContacts());
+  };
+
+  // const showForm = () => {
+  //   setIsFormShown(true);
+  // };
+
+  const closeForm = () => {
+    setIsFormShown(false);
+  };
   return (
     <>
       <ContactForm />
-      <Filter />
-      <ContactList />
+      <>
+        {isListShown ? (
+          <>
+            <Filter />
+            <ContactList />
+            {isFormShown && <ContactForm closeForm={closeForm} />}
+          </>
+        ) : (
+          <Button text="Show users" clickHandler={showContactsList} />
+        )}
+        {error && <p>{error.message}</p>}
+      </>
     </>
   );
 };
